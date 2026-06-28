c57ff4090ef6c123b90991c0f90eca463cedab79
Anupriya-spark <anupriyabarnawal29@gmail.com>
2026-06-28 15:56:18 +0530

    Initial commit

---
+++ agents/github_agent.py
@@ -0,0 +1,12 @@
+import os
+from github import Github
+
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+
+g = Github(GITHUB_TOKEN)
+
+def get_repo_readme(repo_name):
+    repo = g.get_repo(repo_name)
+    file = repo.get_contents("README.md")
+    content = file.decoded_content.decode()
+    return content
---
+++ config.py
@@ -0,0 +1,6 @@
+
+import google.generativeai as genai
+
+genai.configure(api_key=" ")
+
+model = genai.GenerativeModel("models/gemini-2.5-flash")
